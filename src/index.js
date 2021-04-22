98cf808d0da9bcf02c99b4253570af74dcb1a0f9
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-04-22 19:48:42 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import "./styles.css";
+// var変数の定義
+var val1 = "var変数";
+console.log(val1);
+val1 ="var変数を上書き";
+console.log(val1)
+var val1 ="var変数を再宣言";
+console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let変数の定義
+let val2 = "let関数";
+console.log(val2)
+val2 = "let関数を上書き";
+console.log(val2);
+
+// constの定義
+const val3 = "const関数";
+console.log(val3);
+const val3 = "const関を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+const val4 = {
+  name: "hoge",
+  age: 28,
+};
+val4.name = "ほげ";
+val4.addres = "HIROSHIMA";
+console.log(val4);
+
+// 配列、pushで配列に追加する
+const val5 = ["dog","cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
