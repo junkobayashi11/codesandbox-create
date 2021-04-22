c58735de0f45824f3f20b7c75ac965d43359e18c
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-04-22 20:41:42 +0900

    fix: 配列コメントアウト修正

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-分割代入;
-const myProfile = {
-  name: "jun",
-  age: 23
-};
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// 分割代入;
+// const myProfile = {
+//   name: "jun",
+//   age: 23
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
 
-const { name, age } = myProfile;
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
 
+// 配列の分割代入
 const myProfile = ["jun", 23];
 const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
 
 const [name, age] = myProfile;
 const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
