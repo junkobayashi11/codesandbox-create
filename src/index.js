06c8940b335934439ac9ac6c3d13291e7bd3e6d9
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-04-22 20:38:26 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-// アロー関数の使用例
-// 従来の関数の書き方
-// function func1(str){
-//   return str;
-// }
-
-const func1 = function (str) {
-  return str;
+分割代入;
+const myProfile = {
+  name: "jun",
+  age: 23
 };
-console.log(func1("func1です"));
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
 
-// アロー関数
-const func2 = (str) => str;
-console.log(func2("func2です"));
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+const myProfile = ["jun", 23];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
