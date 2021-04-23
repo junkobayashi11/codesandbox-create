250158047711d536e829b55d412cadebefb1047e
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-04-23 16:08:27 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-// 三項演算子
+// 論理演算子の本当の意味を知ろう
+const flag1 = true;
+const flag2 = false;
 
-// ある条件？条件がtrueの時：条件がfalseの時
-// const val1 = 1 > 0 ? "trueです":"falseです";
-// console.log(val1);
+if (flag1 || flag2) {
+  console.log("1か2はtrueになります");
+}
+if (flag1 && flag2) {
+  console.log("1も2もtrueになります");
+}
 
-// const num = 1300;
-// console.log(num.toLocaleString());
+// ||は左側がfalseとなら右側を返す
+const num = "";
+const fee = num || "金額未設定です";
+console.log(fee);
 
-// const formattedNum = typeof num === "number" ? num.toLocaleString():"数値を入力してください";
-// console.log(formattedNum);
-
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
-};
-console.log(checkSum(50, 70));
+// &&は左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
