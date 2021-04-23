c5839046222681eb613ca8dd15bc98b204e79a74
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-04-23 09:33:24 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-// 分割代入;
-// const myProfile = {
-//   name: "jun",
-//   age: 23
-// };
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// デフォルト値、引数など
+const sayHello = (name = "jun") => console.log(`こんにちは${name}さん`);
+sayHello();
 
-// const { name, age } = myProfile;
-// const message2 = `名前は${name}です。年齢は${age}歳です。`;
-
-// 配列の分割代入
-const myProfile = ["jun", 23];
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
