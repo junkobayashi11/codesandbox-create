f4d96dbec2b8b4aa881f53c2d92a7e262ee79c98
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-04-23 13:54:32 +0900

    mapやフィルター学習

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
-// スプレット構文...
-// 配列の展開
-// const arr1 = [1,2];
-// // console.log(arr1);
-// // console.log(...arr1);
+// mapやフィルターを使った配列の処理
+const nameArr = ["田中", "山田", "jun"];
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(`${index+1}番目は${nameArr[index]}です`);
+// }
 
-// const sumFunc = (num1, num2) => console.log(num1 + num2);
-// sumFunc(arr1[0],arr1[1]);
-// sumFunc(...arr1);
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
 
-// 一つにまとめる
-// const arr2 = [1,2,3,4,5];
-// const [num1,num2,...arr3] = arr2;
-// console.log(num1);
-// console.log(num2);
-// console.log(arr3);
+// nameArr.map((name, index)=>console.log(`${index+1}番目は${name}です。`));
 
-// 配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num)=>{
+//   return num % 2 === 0;
+// });
 
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr6);
-console.log(arr4);
+// console.log(newNumArr);
+const newNameArr = nameArr.map((name) => {
+  if (name === "jun") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
 
-// const arr7 = [...arr4, ...arr5];
-// console.log(arr7);
-
-// const arr8 = arr4;
-// arr8[0] = 100;
-// console.log(arr8);
+console.log(newNameArr);
